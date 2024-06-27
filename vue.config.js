0b9dc55a610513e6080332f2ffb3bc1405efbe76
Axi404 <axihelloworld@gmail.com>
2024-06-27 21:59:04 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/cs-baoyan-deadlines/'
+    ? '/cs-baoyan-ddl/'
     : '/'
 };
 
